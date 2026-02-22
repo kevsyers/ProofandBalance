a484f19fb25f04dcd0602b46503a26471ba1b176
Kevin <kevsyers@yahoo.com>
2026-02-21 21:29:13 -0500

    Fresh PB site

--- nav.js
+++ nav.js
@@ -1,9 +1,12 @@
 const navbar = document.querySelector(".navbar");
+const logoImg = document.querySelector(".logo img");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
         navbar.classList.add("scrolled");
+        logoImg.src = "./assets/images/PB-logo.svg";
     } else {
         navbar.classList.remove("scrolled");
+        logoImg.src = "./assets/images/PB-logo-reverse.svg";
     }
 });
